795fa1c3320b88385f480fe14e1741c4f4502c51
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-06-26 00:53:31 +0530

    format with prettier

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,38 +1,38 @@
-import Accounts from "../components/accounts";
+import Accounts from '../components/accounts';
 
 const INPUT = [
-    {
-        id: 'fname',
-        label: 'First Name',
-        type: 'text',
-        name: 'fname',
-        autofocus: true,
-    },
-    {
-        id: 'lname',
-        label: 'Last Name',
-        type: 'text',
-        name: 'lname',
-        autofocus: false,
-    },
-    {
-        id: 'email',
-        label: 'Email Address',
-        type: 'email',
-        name: 'email',
-        autofocus: false,
-    },
-    {
-        id: 'password',
-        label: 'Password',
-        type: 'password',
-        name: 'password',
-        autofocus: false,
-    }
+	{
+		id: 'fname',
+		label: 'First Name',
+		type: 'text',
+		name: 'fname',
+		autofocus: true,
+	},
+	{
+		id: 'lname',
+		label: 'Last Name',
+		type: 'text',
+		name: 'lname',
+		autofocus: false,
+	},
+	{
+		id: 'email',
+		label: 'Email Address',
+		type: 'email',
+		name: 'email',
+		autofocus: false,
+	},
+	{
+		id: 'password',
+		label: 'Password',
+		type: 'password',
+		name: 'password',
+		autofocus: false,
+	},
 ];
 
-function Login(){
-    return <Accounts items={INPUT} action={"sign up"} />
+function Login() {
+	return <Accounts items={INPUT} action={'sign up'} />;
 }
 
 export default Login;
