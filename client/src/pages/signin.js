5be5bb83efd7b4fce82eab8399179707bf1932e6
kanishkguptagit <1905902@kiit.ac.in>
2021-06-25 21:41:21 +0530

    outsourced login pages to one accounts

--- client/src/pages/signin.js
+++ client/src/pages/signin.js
@@ -1,82 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import Accounts from "../components/accounts";
 
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
+const INPUT = [
+    {
+        id: 'email',
+        label: 'Email',
+        type: 'email',
+        name: 'email',
+        autofocus: true,
+    },
+    {
+        id: 'password',
+        label: 'Password',
+        type: 'password',
+        name: 'password',
+        autofocus: false,
+    }
+];
 
-import accountsStyles from "../styles/accounts";
-
-export default function Signin() {
-  const classes = accountsStyles();
-
-  return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />      
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}></Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs justify="left">
-                <Link to="#" variant="body2">
-                  {"Forgot password?"}
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link to="/signup" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>            
-          </form>
-        </div>
-      </Grid>
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-    </Grid>
-  );
+function Login(){
+    return <Accounts items={INPUT} action={"sign in"} />
 }
+
+export default Login;
