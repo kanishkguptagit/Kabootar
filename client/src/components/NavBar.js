4b0564da0e886ec761f5e12b069d41ce41f6e2a7
kanishkguptagit <1905902@kiit.ac.in>
2021-06-26 17:44:43 +0530

    dashboard complete

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,8 +3,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-// import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
 
 import NavStyles from '../styles/NavBar';
 
@@ -15,11 +13,8 @@ export default function NavBar() {
 		<div className={classes.root}>
 			<AppBar position="sticky">
 				<Toolbar>
-					{/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton> */}
 					<Typography variant="h6" className={classes.title}>
-						Kabootar
+						kabootar
 					</Typography>
 					<Button color="inherit">Contact</Button>
 					<Button color="inherit">Login</Button>
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,15 +1,32 @@
 import { Fragment } from 'react';
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 import classes from '../styles/Landing.module.css';
 
-function Landing(){
-    return <Fragment>
-        <section >
-            <div className={classes.image}>
-            <img src="https://source.unsplash.com/G_lwAp0TF38/1800x1800" />
-            </div>
-        </section>
-    </Fragment>
+function Landing() {
+
+    const continueButton = "/signin";
+
+	return (
+		<Fragment>
+			<div className={`${classes.image} ${classes.centered}`}>
+				<img src="/logos/icon.png" alt="Logo" />
+			</div>
+			<div className={`${classes.heading} ${classes.centered}`}>kabootar</div>
+			<div className={classes.centered}>
+                <Link to={continueButton}>
+				<Button
+					variant="outlined"
+					color="primary"
+					size="large"
+					style={{ textTransform: 'none' }}>
+					Continue to kabootar
+				</Button>
+                </Link>
+			</div>
+		</Fragment>
+	);
 }
 
 export default Landing;
