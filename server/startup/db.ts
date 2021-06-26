5e9fd7cd344faba9a0ad4e141dac4052c5de27b1
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-06-26 15:24:56 +0530

    add db startup

---
+++ server/startup/db.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import chalk from 'chalk';
+
+export default function connectToDB(): void {
+	console.log(chalk.white.bgYellow('attempting connection to database'));
+
+	const uri = process.env.ATLAS_URI;
+	if (!uri) {
+		console.log(chalk.white.bgRedBright('MONGO ALTAS URI not found'));
+		return;
+	}
+
+	mongoose.connect(uri, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	});
+
+	mongoose.connection.once('open', () => {
+		console.log(chalk.whiteBright.bgGreenBright('connection to database successful'));
+	});
+}
