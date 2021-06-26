6c33b6215ac3d1f6918e442c4ee2accc6377fc57
kanishkguptagit <56789607+kanishkguptagit@users.noreply.github.com>
2021-06-26 20:03:02 +0530

    [IMPROVE] Added History Page (#14)
    * added dashboard
    
    * added comma
    
    * dashboard complete
    
    * added functionality to navbar
    
    * added history page
    
    * resolve the errors
    
    Co-authored-by: aditya-mitra <ultrapokeinstinct@gmail.com>

--- client/src/components/dashboard/Deposits.js
+++ client/src/components/dashboard/Deposits.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Title from './Title';
 
-function preventDefault(event) {
-  event.preventDefault();
-}
+// function preventDefault(event) {
+//   event.preventDefault();
+// }
 
 const useStyles = makeStyles({
   depositContext: {
