56b325bae0ae4c3f440e522a1728fcc7728f8fa5
kanishkguptagit <1905902@kiit.ac.in>
2021-06-26 20:43:46 +0530

    outsourced dashboard and hsitory from a single component

---
+++ client/src/components/Layout.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import clsx from 'clsx';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { mainListItems, secondaryListItems } from './dashboard/listItems';
+// import Chart from '../components/dashboard/Chart';
+import Deposits from './dashboard/Deposits';
+import Orders from './dashboard/Orders';
+import layoutStyles from '../styles/Layout';
+
+export default function Layout(props) {
+  const classes = layoutStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+            Mails
+          </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>{mainListItems}</List>
+        <Divider />
+        <List>{secondaryListItems}</List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+
+            {/* Chart */}
+            { props.chart.enable && <Grid item xs={12} md={8} lg={9}>
+              <Paper className={fixedHeightPaper}>
+                {/* <Chart /> */}
+              </Paper>
+            </Grid>}
+
+            {/* Recent Deposits */}
+            { props.block.enable && <Grid item xs={12} md={4} lg={3}>
+              <Paper className={fixedHeightPaper}>
+                <Deposits detail={props.block.detail} />
+              </Paper>
+            </Grid>}
+
+            {/* Recent Orders */}
+            { props.list.enable && <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <Orders item={props.list.items} />
+              </Paper>
+            </Grid>}
+          </Grid>          
+        </Container>
+      </main>
+    </div>
+  );
+}
