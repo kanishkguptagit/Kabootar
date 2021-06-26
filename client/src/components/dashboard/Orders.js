e47d1aae0fd85ecc4ad61dca98273968eb82fa28
kanishkguptagit <1905902@kiit.ac.in>
2021-06-26 14:45:47 +0530

    added comma

--- client/src/components/dashboard/Orders.js
+++ client/src/components/dashboard/Orders.js
@@ -14,11 +14,11 @@ function createData(id, date, schedule, recipient, subject ) {
 }
 
 const rows = [
-  createData(0, '16 Mar, 2019', '20 Mar, 2019: 10:40 am', 'Mitra Choda', 'Mitra being Mitra with his chutiyap'),
-  createData(1, '16 Mar, 2019', '20 Mar, 2019: 10:40 am', 'Mitra Choda', 'Chutiyap'),
-  createData(2, '16 Mar, 2019', '20 Mar, 2019: 10:40 am', 'Mitra Choda', 'Chutiyap'),
-  createData(3, '16 Mar, 2019', '20 Mar, 2019: 10:40 am', 'Mitra Choda', 'Chutiyap'),
-  createData(4, '16 Mar, 2019', '20 Mar, 2019: 10:40 am', 'Mitra Choda', 'Chutiyap'),
+  createData(0, '16 Mar, 2019', '20 Mar, 2019, 10:40 am', 'Mitra Choda', 'Mitra being Mitra with his chutiyap'),
+  createData(1, '16 Mar, 2019', '20 Mar, 2019, 10:40 am', 'Mitra Choda', 'Chutiyap'),
+  createData(2, '16 Mar, 2019', '20 Mar, 2019, 10:40 am', 'Mitra Choda', 'Chutiyap'),
+  createData(3, '16 Mar, 2019', '20 Mar, 2019, 10:40 am', 'Mitra Choda', 'Chutiyap'),
+  createData(4, '16 Mar, 2019', '20 Mar, 2019, 10:40 am', 'Mitra Choda', 'Chutiyap'),
 ];
 
 function preventDefault(event) {
