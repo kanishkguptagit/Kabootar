a29c09bb8eeade44332743e10b5910abac61c9b7
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-06-27 15:26:31 +0530

    headers content type

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -18,22 +18,23 @@ const INPUT = [
 ];
 
 function Signin() {
-
-	const signinHandler = async (inputs) => {
+	const signinHandler = async inputs => {
 		const email = inputs[0].value;
 		const password = inputs[1].value;
-		
+
 		const response = await fetch('http://localhost:5000/users/signin', {
-				method: 'POST',
-				body: JSON.stringify({
-					email: email,
-					password: password,
-				})
-			}
-		)
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email: email,
+				password: password,
+			}),
+		}).then(res => res.json());
 
 		console.log(response);
-	}
+	};
 
 	return <Accounts items={INPUT} action={'sign in'} login={signinHandler} />;
 }
