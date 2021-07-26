a290f6ef83756d588dd05bfb6728cf1d00be9209
kanishkguptagit <1905902@kiit.ac.in>
2021-07-26 14:19:23 +0530

    aligned task items

--- client/src/pages/Task.js
+++ client/src/pages/Task.js
@@ -1,12 +1,73 @@
 import Layout from '../components/Layout';
+import { TextField } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-function Task(){
+import TaskChart from '../components/TaskChart';
 
-    return(
-        <Layout title={'Task'}>
+const DUMMY_DATA = [
+	{
+		id: 't1',
+		date: 'Mon Jul 26 2021 - 1:11:55 PM',
+		task: 'Hello World!! Assignment to complete henceforth',
+	},
+	{
+		id: 't2',
+		date: 'Mon Jul 26 2021 - 1:11:55 PM',
+		task: 'Classes to attend and not to miss hello world!!',
+	},
+];
 
-        </Layout>
-    )
-};
+const useStyles = makeStyles({
+	containerBox: {
+		display: 'flex',
+		justifyContent: 'center',
+		width: '100%',
+		textAlign: 'center',
+        margin: '3%'
+	},
+	text: {
+		flexShrink: 1,
+		marginRight: '1%',
+        width: '100%'
+	},
+	add: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		float: 'right',
+	},
+    button: {
+        height: '100%',
+        width: '',
+    }
+});
+
+function Task() {
+	const classes = useStyles();
+
+	return (
+		<Layout title={'Task'}>
+			<div className={classes.containerBox}>
+				<div className={classes.text}>
+					<TextField
+						id="outlined-multiline-static"
+						label="Task"
+						multiline
+						placeholder="Add tasks here"
+						fullWidth
+						variant="outlined"
+					/>
+				</div>
+				<div className={classes.add}>
+					<Button variant="outlined" size="large" color="primary" className={classes.button}>
+						Add
+					</Button>
+				</div>
+			</div>
+			<TaskChart item={DUMMY_DATA} />
+		</Layout>
+	);
+}
 
 export default Task;
